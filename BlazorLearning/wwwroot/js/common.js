f1673370e2de10d6c8a779676cb248ea1330bbfd
Peter Bagi <peti.bagi@icloud.com>
2024-09-07 14:37:12 +0200

    toaster/sweetalert added

---
+++ BlazorLearning/wwwroot/js/common.js
@@ -0,0 +1,31 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
+
+window.showSwal = function (type, message) {
+    if (type == "success") {
+
+        Swal.fire({
+            title: "Ez szép munka!",
+            text: message,
+            icon: "success"
+        });
+       
+    }
+
+    if (type == "error") {
+
+        Swal.fire({
+            title: "Ez elrontott munka!",
+            text: message,
+            icon: "error"
+        });
+        
+    }
+}
